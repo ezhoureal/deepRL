5bb095e56292e7ccc3924804be45b47101ad8b91
Tianer Zhou <ezhoureal@gmail.com>
2025-03-18 17:32:13 +0800

    tested on pendulum

--- hw3/cs285/scripts/run_hw3_sac.py
+++ hw3/cs285/scripts/run_hw3_sac.py
@@ -107,6 +107,8 @@ def run_training_loop(config: dict, logger: Logger, args: argparse.Namespace):
                     logger.log_scalar(v, k, step)
                     logger.log_scalars
                 logger.flush()
+                # print(f'q_values = {update_info["q_values"]}, entropy = {update_info["entropy"]}')
+
 
         # Run evaluation
         if step % args.eval_interval == 0:
